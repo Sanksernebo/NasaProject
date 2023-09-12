3f8695c9b58e72383e1ca668fe8ca5482da7d4ae
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2023-09-12 13:56:43 +0300

    Login test, API

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Login from "../Login/login";
+import "../../scss/App.css";
+
+function App() {
+    <Login></Login>
+}
+
+export default App;
